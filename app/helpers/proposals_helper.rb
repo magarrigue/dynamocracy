ec7de1f9456e57ccd4e5aad00e9b0911ff85b6a6
Marc-Antoine Garrigue <mag@octo.com>
2010-08-19 01:17:34 +0200

    cleaning proposal index

---
+++ app/helpers/proposals_helper.rb
@@ -0,0 +1,29 @@
+module ProposalsHelper
+  def mine?(proposal)
+    proposal.user_id == current_user.id
+  end
+  
+  def withdrawable?(proposal)
+      mine?(proposal) && (proposal.full_status == 'open' || proposal.full_status == 'pending')
+  end
+  
+  def votable?(proposal)
+    proposal.full_status == 'open' && @my_signatures.none?{|s| s.proposal_id==proposal.id}
+  end
+  
+  def actionable?(proposal)
+    withdrawable?(proposal) || votable?(proposal) 
+  end
+  
+  def result?(proposal)
+  %w(closed cancelled).include?(proposal.full_status)
+  end
+  
+  def results?
+   @proposals.any?{|p| result? p } 
+  end
+  def actionables?
+   @proposals.any?{|p|actionable?(p)}
+  end
+end
+
