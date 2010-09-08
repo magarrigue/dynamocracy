a628a502afd71842575686df36617c7820ef005e
Marc-Antoine Garrigue <mag@octo.com>
2010-09-08 12:16:10 +0200

    gems

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'inherited_resources'
   config.gem 'haml'
-  config.gem 'formtastic'
   config.gem 'units'
   config.gem 'searchlogic'
   config.gem 'will_paginate'
