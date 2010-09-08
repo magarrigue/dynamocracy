133fc5d257d6aee045f37951ddc80ff065f5ba3d
Marc-Antoine Garrigue <mag@octo.com>
2010-09-08 18:12:51 +0200

    styling and stuff

--- app/models/news_mailer.rb
+++ app/models/news_mailer.rb
@@ -3,7 +3,7 @@ class NewsMailer < ActionMailer::Base
    def news(user)
       recipients user.email
       from "no-reply@#{ENV['host']}"
-      subject "News from your Dynamocracy crews : #{Time.now}"
+      subject "News from your Dynamocracy crews : #{Time.now.ctime}"
       fetch_news_for_user(user)
       @user = user
       content_type "text/html"
