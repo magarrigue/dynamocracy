d78b2db2550cdda3092baf2024048012ddd10db9
Marc-Antoine Garrigue <mag@octo.com>
2010-09-09 09:41:11 +0200

    change from invit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "no-reply@dynamocracy.com"
+  config.mailer_sender = "Dynamocracy <no-reply@dynamocracy.com>"
   
   # Configure the content type of DeviseMailer mails (defaults to text/html")
   # config.mailer_content_type = "text/plain"
