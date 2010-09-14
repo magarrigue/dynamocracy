ce462e2f8466cff8ea5965a426b96503d759c2fd
Marc-Antoine Garrigue <mag@octo.com>
2010-09-14 23:56:30 +0200

    adding mail host and crew configuration

--- app/models/news_mailer.rb
+++ app/models/news_mailer.rb
@@ -2,8 +2,8 @@ class NewsMailer < ActionMailer::Base
   include News
    def news(user)
       recipients user.email
-      from "Dynamocracy News <no-reply@#{ENV['host']}>"
-      subject "News from your Dynamocracy crews : #{Time.now.ctime}"
+      from "#{ENV['CREW']||'Dynamocracy'} News <no-reply@#{ENV['host']}>"
+      subject "News from your #{ENV['CREW']||'Dynamocracy'} crews : #{Time.now.ctime}"
       fetch_news_for_user(user)
       @user = user
       content_type "text/html"
