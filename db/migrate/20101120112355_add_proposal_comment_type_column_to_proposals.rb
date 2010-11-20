4f42a4fb04749c41afedfe22aaff7a915c46697b
Marc-Antoine Garrigue <mag@octo.com>
2010-11-20 13:16:04 +0100

    adding public comment option

--- db/migrate/20101120112355_add_proposal_comment_type_column_to_proposals.rb
+++ db/migrate/20101120112355_add_proposal_comment_type_column_to_proposals.rb
@@ -1,8 +1,7 @@
 class AddProposalCommentTypeColumnToProposals < ActiveRecord::Migration
   def self.up
     add_column :proposals, :public_comments_allowed, :boolean, :default=>true, :null=>false
-    Proposal.find_each{|p| p.public_comments_allowed=false}
-    
+    Proposal.find_each{|p| p.public_comments_allowed=false; p.save false;}
   end
 
   def self.down
