597848795363c515a3f4598ca433917564a358ad
Marc-Antoine Garrigue <mag@octo.com>
2010-11-20 15:55:47 +0100

    details and score on members, link on crews

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,12 @@ module ApplicationHelper
     "in #{distance_of_time_in_words_to_now date}"
   end
 
+  def member_proposal_contribution_score(membership)  
+    proposals = Proposal.user_id_equals(membership.user_id).crew_id_equals(membership.crew_id)
+    score = 0
+    proposals.each{|p| score = p.yes_count+p.support_count}
+    score
+  end
  
 
 end
